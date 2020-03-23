6d601409a909459c751373d918c548a4ce1c49a2
Greta Simonyan <gsimonyan228@gmail.com>
2020-03-23 18:11:44 +0400

    create project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component} from "react";
+// import {hot} from "react-hot-loader";
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+// export default hot(module)(App);
